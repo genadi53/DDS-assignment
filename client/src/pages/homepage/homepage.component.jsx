1c96726d73e3333895d1c2eee3cd353b5aff07b7
genadi53 <kotka5353@gmail.com>
2021-07-27 15:16:48 +0300

    fix the issue of updating unmounted component

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import PartComponent from "../../components/part/part.component";
 const HomePage = () => {
   const [parts, setParts] = useState([]);
+  const isMounted = useRef(true);
+
   useEffect(() => {
+    isMounted.current = true;
     axios({
       method: "get",
       url: "http://localhost:5000/api/parts",
     })
       .then((res) => {
         //console.log(res);
-        setParts(res.data);
+        if (isMounted.current) {
+          setParts(res.data);
+        }
       })
       .catch((err) => console.log(err));
-  }, []);
+    return () => (isMounted.current = false);
+  }, [parts]);
 
   return (
     <div className="row">
